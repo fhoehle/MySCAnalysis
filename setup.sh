aac42e51649c0a0379bb364517b25f413dbd6aea
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-07-08 16:59:05 +0200

    function for git packages

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-
+function getGitPackage {
+echo "getting "$1
+if [ -d "$1" ]; then
+  cd $1
+  git fetch
+else
+  git clone git@github.com:fhoehle/$1.git
+  cd $1
+fi
+  
+}
 cmsswVer=CMSSW_4_2_8_patch7
 
 
@@ -14,14 +24,12 @@ fi
 cd $CMSSW_BASE
 set -e
 # di lep selection
-git clone git@github.com:fhoehle/DiLeptonicSelection.git
-cd DiLeptonicSelection
+getGitPackage "DiLeptonicSelection" 
 git checkout V00-04
 ./install/installMyFWK.sh
-# tools
 cd $CMSSW_BASE
-git clone git@github.com:fhoehle/MyCMSSWAnalysisTools.git
-cd MyCMSSWAnalysisTools
+# tools
+getGitPackage "MyCMSSWAnalysisTools"
 git checkout V00-03
 #
 cd $CMSSW_BASE

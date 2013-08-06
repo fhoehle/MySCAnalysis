9e566810a7dce09c1512c19cb7b11b22543fcaf1
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-08-06 10:51:05 +0200

    added signal processing script

--- runSignal/runAnalysis.py
+++ runSignal/runAnalysis.py
@@ -1,4 +1,8 @@
 # run cmssw analysis
+def removeAddOptions(toRemove,options):
+  for key in toRemove:
+   options = re.sub(key+'=[^\ ]*','',options)
+  return options
 import FWCore.ParameterSet.Config as cms
 import sys,imp,subprocess,os,getopt,re,signal
 sys.path.append(os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools/Tools')
@@ -24,16 +28,40 @@ for opt,arg in opts:
 options ={}
 options["maxEvents"]=1000
 options["outputPath"]=os.getenv("PWD")
+import datetime,time
+timeStamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
 for opt in addOptions.split():
   reOpt = re.match('([^=]*)=([^=]*)',opt)
   if options.has_key(reOpt.group(1)):
     options[reOpt.group(1)]=reOpt.group(2)
 if options["outputPath"] != os.getenv("PWD"):
   if not noTimeStamp:
-    import datetime,time  
-    options["outputPath"]= os.path.realpath(options["outputPath"])+"_"+datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')+os.path.sep 
+    options["outputPath"]= os.path.realpath(options["outputPath"])+"_"+timeStamp+os.path.sep 
     print options["outputPath"]
-processSample =  myTools.processSample('../../DiLeptonicSelection/patRefSel_diLep_cfg.py')
+## preparing cfg with additional options
+#make tmp copy
+import shutil
+cfg = '../../DiLeptonicSelection/patRefSel_diLep_cfg.py'
+tmpCfg = options["outputPath"]+ os.path.splitext(os.path.basename(cfg))[0]+"_"+timeStamp + os.path.splitext(os.path.basename(cfg))[1]
+os.makedirs(os.path.dirname(tmpCfg))
+shutil.copyfile(cfg,tmpCfg)
+remainingOptions = removeAddOptions(options.keys(),addOptions)
+if remainingOptions != '' and not remainingOptions.isspace():
+  tmpCfgDumpPython = os.path.splitext(tmpCfg)[0]+"_addedDumpLine"+os.path.splitext(tmpCfg)[1]
+  tmpCfgAddLine = open(tmpCfg,"a");tmpCfgAddLine.write('myTmpFile = open ("'+tmpCfgDumpPython+'","w"); myTmpFile.write(process.dumpPython()); myTmpFile.close()');tmpCfgAddLine.close()
+  import subprocess
+  buildFile = subprocess.Popen(["python "+tmpCfg+" "+removeAddOptions(options.keys(),addOptions)],shell=True,stdout=subprocess.PIPE,env=os.environ)
+  buildFile.wait()
+  errorcode = buildFile.returncode
+  if errorcode != 0:
+    sys.exit("failed building config with "+str(errorcode))
+    cfg = tmpCfgDumpPython
+  else:
+    print "python cfg creation done"
+else:
+  cfg = tmpCfg
+### start processing sample
+processSample = myTools.processSample(cfg)
 for postfix,filename in [ (p,f) for p,f in signalSamples.testFiles.iteritems()]: 
   sample = myTools.sample(filename,postfix,int(options["maxEvents"]))
   print "processing ",postfix," ",filename,"  ",options["outputPath"]

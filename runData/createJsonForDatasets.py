1c2f701d1e6cc8b471bb23ad0ba974f7f7b93d97
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-09-30 16:10:01 +0200

    added lumi calcutlation and comparison

---
+++ runData/createJsonForDatasets.py
@@ -0,0 +1,18 @@
+import sys,os
+sys.path.append(os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools')
+import MyDASTools.dasTools as dasTools
+dasClient = dasTools.myDasClient()
+dasClient.limit=0
+datasets = [
+  #{'label':'singleMuRun2011APromptV4','name':'/SingleMu/Run2011A-PromptReco-v4/AOD run = 167746','jsonFilename':'singleMuRun2011APromptV4_JSON.txt'}
+  {'label':'singleMuRun2011APromptV4','name':'/SingleMu/Run2011A-PromptReco-v4/AOD','jsonFilename':'singleMuRun2011APromptV4_JSON.txt'}
+  ,{'label':'singleMuRun2011AMay10ReReco','name':'/SingleMu/Run2011A-May10ReReco-v1/AOD','jsonFilename':'singleMuRun2011AMay10ReReco_JSON.txt'}
+  ,{'label':'singleMuRun2011A05Aug','name':'/SingleMu/Run2011A-05Aug2011-v1/AOD','jsonFilename':'singleMuRun2011A05Aug_JSON.txt'}
+  ,{'label':'singleMuRun2011A08Nov','name':'/SingleMu/Run2011A-08Nov2011-v1/AOD','jsonFilename':'singleMuRun2011A08Nov_JSON.txt'}
+]
+locPrefix = ''
+for d in datasets:
+  jsonDict = dasClient.getJsonOfDataset(d['name'])
+  JSONfilename = locPrefix+d['jsonFilename']
+  jsonDict.writeJSON(JSONfilename)
+  print 'written ',JSONfilename
---
+++ runData/dataSamples.py
@@ -0,0 +1,42 @@
+/SingleMu/Run2011A-PromptReco-v6/AOD
+/SingleMu/Run2011A-PromptReco-v4/AOD
+/SingleMu/Run2011A-May10ReReco-v1/AOD
+/SingleMu/Run2011A-08Nov2011-v1/AOD
+/SingleMu/Run2011A-05Aug2011-v1/AOD
+/SingleMu/Run2011A-03Oct2011-v1/AOD
+dataFiles = {
+  'SingleMu__Run2011A-PromptReco-v4__AOD' : {
+    'localFile':None
+    ,'xSec':None
+    ,'datasetName':'/SingleMu/Run2011A-PromptReco-v4/AOD'
+    ,'label':'Data_Run2011A_PromptReco-v4'
+    ,"color":0
+  }
+  ,'SingleMu__Run2011A-May10ReReco-v1__AOD':{
+     'localFile':None
+    ,'xSec':None
+    ,'datasetName':'/SingleMu/Run2011A-May10ReReco-v1/AOD'
+    ,'label':'Data_Run2011A_May10ReReco-v1'
+    ,"color":0
+ 
+  }
+  ,'SingleMu__Run2011A-05Aug2011-v1__AOD':{
+    'localFile':None
+    ,'xSec':None
+    ,'datasetName':'/SingleMu/Run2011A-05Aug2011-v1/AOD'
+    ,'label':'Data_Run2011A_05Aug2011-v1'
+    ,"color":0
+
+  }
+  ,'SingleMu__Run2011A-08Nov2011-v1__AOD':{
+    'localFile':None
+    ,'xSec':None
+    ,'datasetName':'/SingleMu/Run2011A-08Nov2011-v1/AOD'
+    ,'label':'Data_Run2011A_08Nov2011-v1'
+    ,"color":0
+
+  }
+
+
+
+}

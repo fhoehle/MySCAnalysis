5880b6b049ad56a9b62e74df1c29e69ce4977d3d
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-10-01 12:23:22 +0200

    added datasets removed unnecessary ones

--- runData/createJsonForDatasets.py
+++ runData/createJsonForDatasets.py
@@ -9,6 +9,8 @@ datasets = [
   ,{'label':'singleMuRun2011AMay10ReReco','name':'/SingleMu/Run2011A-May10ReReco-v1/AOD','jsonFilename':'singleMuRun2011AMay10ReReco_JSON.txt'}
   ,{'label':'singleMuRun2011A05Aug','name':'/SingleMu/Run2011A-05Aug2011-v1/AOD','jsonFilename':'singleMuRun2011A05Aug_JSON.txt'}
   ,{'label':'singleMuRun2011A08Nov','name':'/SingleMu/Run2011A-08Nov2011-v1/AOD','jsonFilename':'singleMuRun2011A08Nov_JSON.txt'}
+  ,{'label':'singleMuRun2011APromptV6','name':'/SingleMu/Run2011A-PromptReco-v6/AOD','jsonFilename':'singleMuRun2011APromptV6_JSON.txt'}
+  ,{'label':'singleMuRun2011BPromptV1','name':'/SingleMu/Run2011B-PromptReco-v1/AOD','jsonFilename':'singleMuRun2011BPromptV1_JSON.txt'}
 ]
 locPrefix = ''
 for d in datasets:

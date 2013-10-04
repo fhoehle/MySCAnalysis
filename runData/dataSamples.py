0b9f4eb0af775ce777077ab63b6c19064c96bae0
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-10-04 23:38:41 +0200

    corrected dataSamples

--- runData/dataSamples.py
+++ runData/dataSamples.py
@@ -1,11 +1,12 @@
-/SingleMu/Run2011A-PromptReco-v6/AOD
-/SingleMu/Run2011A-PromptReco-v4/AOD
-/SingleMu/Run2011A-May10ReReco-v1/AOD
-/SingleMu/Run2011A-08Nov2011-v1/AOD
-/SingleMu/Run2011A-05Aug2011-v1/AOD
-/SingleMu/Run2011A-03Oct2011-v1/AOD
 dataFiles = {
-  'SingleMu__Run2011A-PromptReco-v4__AOD' : {
+  'SingleMu__Run2011A-PromptReco-v6__AOD':{
+    'localFile':None
+    ,'xSec':None
+    ,'datasetName':'/SingleMu/Run2011A-PromptReco-v6/AOD'
+    ,'label':'Data_Run2011A_PromptReco-v6'
+    ,'color':0
+  }
+  ,'SingleMu__Run2011A-PromptReco-v4__AOD' : {
     'localFile':None
     ,'xSec':None
     ,'datasetName':'/SingleMu/Run2011A-PromptReco-v4/AOD'
@@ -28,15 +29,11 @@ dataFiles = {
     ,"color":0
 
   }
-  ,'SingleMu__Run2011A-08Nov2011-v1__AOD':{
+  ,'SingleMu__Run2011B-PromptReco-v1__AOD':{
     'localFile':None
     ,'xSec':None
-    ,'datasetName':'/SingleMu/Run2011A-08Nov2011-v1/AOD'
-    ,'label':'Data_Run2011A_08Nov2011-v1'
+    ,'datasetName':'/SingleMu/Run2011B-PromptReco-v1/AOD'
+    ,'label':'Data_Run2011B_PromptReco-v1'
     ,"color":0
-
   }
-
-
-
 }

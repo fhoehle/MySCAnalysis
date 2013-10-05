d01d3354e638d99c754d6167e82d0d6063c909a0
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-10-05 20:04:57 +0200

    added jsons and merge one with compareJSON.py

--- runData/runAnalysis.py
+++ runData/runAnalysis.py
@@ -7,6 +7,6 @@ sys.path.append(os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools/Tools')
 import  cmsswAnalysisTools
 import dataSamples
 cfg = '../../DiLeptonicSelection/patRefSel_diLep_cfg.py'
-myAnalysis = cmsswAnalysisTools.cmsswAnalysis(signalSamples.dataDatasets,cfg)
+myAnalysis = cmsswAnalysisTools.cmsswAnalysis(dataSamples.dataDatasets,cfg)
 myAnalysis.readOpts()
-#myAnalysis.startAnalysis()
+myAnalysis.startAnalysis()

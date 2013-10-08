314dc4a880e7455f051c810eb64c49724da17826
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-10-08 23:46:40 +0200

    done runRange management

--- runData/test.py
+++ runData/test.py
@@ -1,31 +1,42 @@
 class test(object):
   def __init__(self,l):
-    self.listR = l
+    from copy import deepcopy
+    self.listR = deepcopy(l)
   def listOfMins(self):
     self.listR.sort(key=lambda r:r.min)
     self.mins=list(set([ r.min for r in self.listR]))
+    self.maxs=[ r.max for r in self.listR];self.maxs.sort();self.maxs=list(set(self.maxs))
     self.openRanges = [r for r in self.listR if r.min==self.mins[0]]
     for r in self.openRanges:
       self.listR.remove(r)
     #print ([r.__dict__ for r in self.openRanges])
-    self.maxs=[ r.max for r in self.listR];self.maxs.sort();self.maxs=list(set(self.maxs))
+    print "maxs ",self.maxs
     self.lowerLimit=self.mins.pop(0)
     self.borders=list(set(self.mins+self.maxs));self.borders.sort();lastLower=None
+    self.ranges=[]
+    print "borders ",self.borders
     while len(self.borders) != 0:
-      print "test ",self.borders[0],",",self.mins[0],",",self.maxs[0]
-      if self.borders[0] == self.mins[0]:
+      print "test ",self.borders[0],",",self.mins,",",self.maxs," lowerLimit ",self.lowerLimit
+      if len(self.mins) and self.borders[0] == self.mins[0]:
         print "lowerLimit  ",self.lowerLimit," min -1 ",self.mins[0] - 1
         for r in self.listR:
           if r.min == self.borders[0]:
             self.openRanges.append(r)
             self.listR.remove(r) 
+        self.ranges.append([self.lowerLimit,self.mins[0] - 1])
+        self.lowerLimit = int(self.mins[0])
+        self.mins.remove(self.mins[0])
       elif self.borders[0] == self.maxs[0]:
         print "lowerLimit  ",self.lowerLimit," max ",self.maxs[0]
         for r in self.openRanges:
 	  if r.max ==  self.borders[0]:
 	    self.openRanges.remove(r)
-            self.lowerLimit = min( self.openRanges, key=lambda r:r.min).min 
-          
+            #self.lowerLimit = min( self.openRanges, key=lambda r:r.min).min 
+        self.ranges.append([self.lowerLimit,self.maxs[0]])  
+        self.lowerLimit= self.maxs[0]+1
+        if len(self.openRanges) == 0 and len(self.mins):
+          self.lowerLimit =  self.mins[0];self.mins.remove(self.mins[0])
+        self.maxs.remove(self.maxs[0])
         print "not"
       else:
         print "something failed"
@@ -34,6 +45,7 @@ class test(object):
 class runRange(object):
   def __init__(self,min,max):
     self.min=min;self.max=max
+import copy
 r1 = runRange(165209, 178419)
 r2 = runRange(167914, 999999)
 runRanges = [r1,r2]

3ca7d6f7be2a9c1f8c06645975330fd119d5579e
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-10-08 22:40:05 +0200

    next iteration

--- runData/test.py
+++ runData/test.py
@@ -10,13 +10,27 @@ class test(object):
     #print ([r.__dict__ for r in self.openRanges])
     self.maxs=[ r.max for r in self.listR];self.maxs.sort();self.maxs=list(set(self.maxs))
     self.lowerLimit=self.mins.pop(0)
-    self.borders=self.mins+self.maxs
+    self.borders=list(set(self.mins+self.maxs));self.borders.sort();lastLower=None
     while len(self.borders) != 0:
+      print "test ",self.borders[0],",",self.mins[0],",",self.maxs[0]
       if self.borders[0] == self.mins[0]:
-        break
-      else:
+        print "lowerLimit  ",self.lowerLimit," min -1 ",self.mins[0] - 1
+        for r in self.listR:
+          if r.min == self.borders[0]:
+            self.openRanges.append(r)
+            self.listR.remove(r) 
+      elif self.borders[0] == self.maxs[0]:
+        print "lowerLimit  ",self.lowerLimit," max ",self.maxs[0]
+        for r in self.openRanges:
+	  if r.max ==  self.borders[0]:
+	    self.openRanges.remove(r)
+            self.lowerLimit = min( self.openRanges, key=lambda r:r.min).min 
+          
         print "not"
-
+      else:
+        print "something failed"
+      print "lowerLimit ",self.lowerLimit
+      self.borders.pop(0)
 class runRange(object):
   def __init__(self,min,max):
     self.min=min;self.max=max

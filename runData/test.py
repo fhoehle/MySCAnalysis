9ee16dd06b85a573ff941cb236056249a08bf2ad
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-10-09 12:15:48 +0200

    added test and debugging

--- runData/test.py
+++ runData/test.py
@@ -1,59 +1,63 @@
-class test(object):
-  def __init__(self,l):
+class runRangeManager(object):
+  def __init__(self,l,debug=False):
     from copy import deepcopy
     self.listR = deepcopy(l)
-  def listOfMins(self):
+    self.debug=debug
+  def calcTriggerRunRanges(self):
     self.listR.sort(key=lambda r:r.min)
-    self.mins=list(set([ r.min for r in self.listR]));self.mins.sort()
-    self.maxs=list(set([ r.max for r in self.listR]));self.maxs.sort();
-    self.openRanges = [r for r in self.listR if r.min==self.mins[0]]
-    for r in self.openRanges:
-      self.listR.remove(r)
-    #print ([r.__dict__ for r in self.openRanges])
-    print "maxs ",self.maxs
-    print "mins ",self.mins
-    self.lowerLimit=self.mins.pop(0)
-    self.borders=list(set(self.mins+self.maxs));self.borders.sort();lastLower=None
+    from copy import deepcopy
+    runRanges = deepcopy(self.listR)
+    mins=list(set([ r.min for r in runRanges]));mins.sort()
+    maxs=list(set([ r.max for r in runRanges]));maxs.sort();
+    openRanges = [r for r in runRanges if r.min==mins[0]]
+    lowerLimit=mins.pop(0)
+    borders=list(set(mins+maxs));borders.sort();
+    for r in openRanges:
+      runRanges.remove(r)
     self.ranges=[]
-    print "borders ",self.borders
-    while len(self.borders) != 0:
-      print "test ",self.borders[0],",",self.mins,",",self.maxs," lowerLimit ",self.lowerLimit
-      if (len(self.mins) and self.borders[0] == self.mins[0]) or (self.borders[0] == self.maxs[0]):
-        if len(self.mins) and self.borders[0] == self.mins[0]:
-          print "lowerLimit  ",self.lowerLimit," min -1 ",self.mins[0] - 1
-          for r in self.listR:
-            if r.min == self.borders[0]:
-              self.openRanges.append(r)
-              self.listR.remove(r) 
-          self.ranges.append([self.lowerLimit,self.mins[0] - 1])
-          self.lowerLimit = int(self.mins[0])
-          self.mins.remove(self.mins[0])
-        if self.borders[0] == self.maxs[0]:
-          print "lowerLimit  ",self.lowerLimit," max ",self.maxs[0]
-          for r in self.openRanges:
-	    if r.max ==  self.borders[0]:
-	      self.openRanges.remove(r)
-              #self.lowerLimit = min( self.openRanges, key=lambda r:r.min).min 
-          self.ranges.append([self.lowerLimit,self.maxs[0]])  
-          self.lowerLimit= self.maxs[0]+1
-          if len(self.openRanges) == 0 and len(self.mins):
-            self.lowerLimit =  self.mins[0];self.mins.remove(self.mins[0])
-          self.maxs.remove(self.maxs[0])
-          print "not"
+    while len(borders) != 0:
+      if self.debug:
+        print "debug begin borders ",borders," mins ", mins ," maxs ",maxs," lowerLimit ",lowerLimit, " openRanges ",[ [r.min,r.max] for r in openRanges ]," ranges ",self.ranges
+      if (len(mins) and borders[0] == mins[0]) or (borders[0] == maxs[0]):
+        if len(mins) and borders[0] == mins[0]:
+          for r in runRanges:
+            if r.min == borders[0]:
+              openRanges.append(r)
+              runRanges.remove(r) 
+          self.ranges.append([lowerLimit,mins[0] - 1])
+          lowerLimit = int(mins[0])
+          mins.remove(mins[0])
+        if borders[0] == maxs[0]:
+          for r in openRanges:
+	    if r.max ==  borders[0]:
+	      openRanges.remove(r)
+          self.ranges.append([lowerLimit,maxs[0]])  
+          lowerLimit= maxs[0]+1
+          if len(openRanges) == 0 and len(mins):
+            lowerLimit =  mins[0];mins.remove(mins[0])
+            borders.remove(lowerLimit)
+            openRanges = [r for r in runRanges if r.min==lowerLimit]
+          maxs.remove(maxs[0])
       else:
-        print "something failed"
-      print "lowerLimit ",self.lowerLimit
-      self.borders.pop(0)
+        print "something failed, runRangeManager"
+      borders.pop(0)
+      if self.debug:
+        print "debug end borders ",borders," mins ", mins ," maxs ",maxs," lowerLimit ",lowerLimit, " openRanges ",[ [r.min,r.max] for r in openRanges ]," ranges ",self.ranges
+#################################
 class runRange(object):
   def __init__(self,min,max):
     self.min=min;self.max=max
-import copy
-r1 = runRange(165, 178)
-r2 = runRange(180, 190)
-r3 = runRange(167, 182)
-r4 = runRange(200, 220)
-r5 = runRange(182, 183)
-runRanges = [r1,r2,r3,r4,r5]
-t=test(runRanges)
-t.listOfMins()
-
+  def __repr__(self):
+    return 'runRange("%s","%s")' % (self.min, self.max)
+def test():
+  import copy
+  r1 = runRange(165, 178)
+  r2 = runRange(180, 190)
+  r3 = runRange(167, 182)
+  r4 = runRange(200, 220)
+  r5 = runRange(182, 183)
+  runRanges = [r1,r2,r3,r4,r5]
+  t=runRangeManager(runRanges,True)
+  t.calcTriggerRunRanges()
+  print ([ [r.min,r.max] for r in runRanges])
+  print t.ranges

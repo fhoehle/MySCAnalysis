e4e2f4d1c184a86971b755e55560680a8ec0c438
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-10-10 18:27:51 +0200

    better datasets setting concerning lumis per job and lumis

--- runData/dataSamples.py
+++ runData/dataSamples.py
@@ -6,7 +6,7 @@ dataDatasets = {
     ,'label':'Data_Run2011A_PromptReco-v6'
     ,'color':0
     ,"addOptions":"runOnData=True"
-    ,"crabConfig":{"CMSSW":{"number_of_jobs":1500,"lumi_mask":'singleMuRun2011A_all.txt',"total_number_of_lumis" : -1}}
+    ,"crabConfig":{"CMSSW":{"lumis_per_job":5,"lumi_mask":'singleMuRun2011A_all.txt',"total_number_of_lumis" : -1}}
     ,"runRange":"172620-175770"
   }
   ,'SingleMu__Run2011A-PromptReco-v4__AOD' : {
@@ -15,7 +15,7 @@ dataDatasets = {
     ,'datasetName':'/SingleMu/Run2011A-PromptReco-v4/AOD'
     ,'label':'Data_Run2011A_PromptReco-v4'
     ,"color":0
-    ,"crabConfig":{"CMSSW":{"number_of_jobs":1500,"lumi_mask":'singleMuRun2011A_all.txt',"total_number_of_lumis" : -1}}
+    ,"crabConfig":{"CMSSW":{"lumis_per_job":5,"lumi_mask":'singleMuRun2011A_all.txt',"total_number_of_lumis" : -1}}
     ,"runRange":"165071-168437"
   }
   ,'SingleMu__Run2011A-May10ReReco-v1__AOD':{
@@ -24,7 +24,7 @@ dataDatasets = {
     ,'datasetName':'/SingleMu/Run2011A-May10ReReco-v1/AOD'
     ,'label':'Data_Run2011A_May10ReReco-v1'
     ,"color":0
-    ,"crabConfig":{"CMSSW":{"number_of_jobs":1500,"lumi_mask":'singleMuRun2011A_all.txt',"total_number_of_lumis" : -1}}
+    ,"crabConfig":{"CMSSW":{"lumis_per_job":5,"lumi_mask":'singleMuRun2011A_all.txt',"total_number_of_lumis" : -1}}
     ,"runRange":"160329-163869"
  
   }
@@ -34,7 +34,7 @@ dataDatasets = {
     ,'datasetName':'/SingleMu/Run2011A-05Aug2011-v1/AOD'
     ,'label':'Data_Run2011A_05Aug2011-v1'
     ,"color":0
-    ,"crabConfig":{"CMSSW":{"number_of_jobs":1500,"lumi_mask":'singleMuRun2011A_all.txt',"total_number_of_lumis" : -1}}
+    ,"crabConfig":{"CMSSW":{"lumis_per_job":5,"lumi_mask":'singleMuRun2011A_all.txt',"total_number_of_lumis" : -1}}
     ,"runRange":"170053-172619"
 
   }
@@ -44,7 +44,7 @@ dataDatasets = {
     ,'datasetName':'/SingleMu/Run2011B-PromptReco-v1/AOD'
     ,'label':'Data_Run2011B_PromptReco-v1'
     ,"color":0
-    ,"crabConfig":{"CMSSW":{"number_of_jobs":1500,"lumi_mask":'Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt',"total_number_of_lumis" : -1}}
+    ,"crabConfig":{"CMSSW":{"lumis_per_job":5,"lumi_mask":'Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt',"total_number_of_lumis" : -1}}
     ,"runRange":"175832-180296"
   }
 }

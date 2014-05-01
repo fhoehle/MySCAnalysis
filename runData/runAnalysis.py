d449855ecb60f97ffa6912302264907716451932
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-05-01 02:13:11 +0200

    adapted runAnalysis

--- runData/runAnalysis.py
+++ runData/runAnalysis.py
@@ -5,8 +5,10 @@ import sys
 sys.path.append(os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools/Tools')
 import  cmsswAnalysisTools
 import dataSamples
+import dataDatasets
 cfg = os.getenv('CMSSW_BASE')+'/DiLeptonicSelection/patRefSel_diLep_cfg.py'
-myAnalysis = cmsswAnalysisTools.cmsswAnalysis(dataSamples.dataDatasets,cfg)
+#myAnalysis = cmsswAnalysisTools.cmsswAnalysis(dataSamples.dataDatasets,cfg)
+myAnalysis = cmsswAnalysisTools.cmsswAnalysis(dataDatasets.dataDatasets,cfg)
 sys.argv.append('--runOnData')
 myAnalysis.readOpts()
 myAnalysis.startAnalysis()

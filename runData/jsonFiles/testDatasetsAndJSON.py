dec577c0e57b14ba43720ce89352b568776e2db0
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-05-30 19:28:41 +0200

    better dataset python file creation

--- runData/jsonFiles/testDatasetsAndJSON.py
+++ runData/jsonFiles/testDatasetsAndJSON.py
@@ -6,13 +6,29 @@ import imp
 testFile = os.getenv('CMSSW_BASE')+'/MySCAnalysis/runData/diLeptonData.py'
 test = imp.load_source('module.name', testFile)
 dataDatasets = test.dataDatasets
+#####################
+#expecting
+# dataDatasets = {'label':{'datasetJSON':'jsonFileOfDataset'}}
+#
+#######################
+import re
+primDataS = re.compile('/([^\/][^\/]*)/.*')
+dataStreams = set([primDataS.match(d['datasetName']).group(1) for d in  dataDatasets.values()])
+print "dataStream sanity"
+for dataStream in list(dataStreams):
+  import itertools
+  print "testing ",dataStream
+  #dataDatasets["pommesMuEG"]={'datasetJSON':'/disk1/hoehle/CMSSW_4_2_8_patch7/MySCAnalysis/runData/jsonFiles/Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt'}
+  for c in itertools.combinations([(k,i['datasetJSON']) for k,i in dataDatasets.iteritems() if dataStream in k],2):
+    overLap = lumiTools.LumiList(c[0][1]) & lumiTools.LumiList(c[1][1])
+    if len(overLap) != 0 :
+      print "attention ",c[0]," overlap with ",c[1]
+################
 intLumisDatasets ={}
 for d in dataDatasets.values():
   print(d['datasetName'])
   intLumisDatasets[d['datasetName']] = lumiTools.calcLumi(d['datasetJSON'])
-import re
-primDataS = re.compile('/([^\/][^\/]*)/.*')
-dataStreams = set([primDataS.match(k).group(1) for k in intLumisDatasets.keys()])
+############################
 for dataStream in dataStreams:
   print "stream ",dataStream," sum of "," + ".join([k for k in intLumisDatasets.keys() if dataStream in k])," ",sum ([ i for k,i in intLumisDatasets.iteritems() if dataStream in k])/1000/1000
 print "#####################"
@@ -25,7 +41,7 @@ goldJSON = lumiTools.LumiList(filename=testLMask)
 intLumiGolden=lumiTools.calcLumi(testLMask)
 print "int Lumi lumi_mask ",testLMask," ",intLumiGolden/1000/1000
 print "#####################"
-print "test intersection woth golden JSON/lumi_mask"
+print "test intersection with golden JSON/lumi_mask"
 intLumisGolden = {}
 for d in dataDatasets.values():
   print(d['datasetName'])
@@ -37,4 +53,4 @@ os.remove(tmpFile)
 for dataStream in dataStreams:
   summedIntLumi = sum([ i for k,i in intLumisGolden.iteritems() if dataStream in k])
   print " sum of with golden JSON intersections "," + ".join([k for k in intLumisDatasets.keys() if dataStream in k])," ",summedIntLumi/1000/1000
-  print "lost int lumi of golden JSON ",(lumiTools.calcLumi(goldJSON.filename) - summedIntLumi)/1000/1000 
+  print "lost int lumi of golden JSON ",(intLumiGolden - summedIntLumi)/1000/1000 

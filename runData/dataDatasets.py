bd657d97e321e6b2c21764017e3243866f8ced19
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-06-13 00:56:46 +0200

    added correct golden JSONs

--- runData/dataDatasets.py
+++ runData/dataDatasets.py
@@ -33,15 +33,26 @@ dsEMu =[
 #]
 
 dsDoubleMu_16Jan2012 = [
-  '/DoubleMu/Run2011A-16Jan2012-v1/AOD'
-  ,'/DoubleMu/Run2011B-16Jan2012-v1/AOD'
+  ('/DoubleMu/Run2011A-16Jan2012-v1/AOD','MySCAnalysis/runData/jsonFiles/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON_v2.txt')
+  ,('/DoubleMu/Run2011B-16Jan2012-v1/AOD','MySCAnalysis/runData/jsonFiles/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON_v2.txt')
 ]
 
+dsDoubleE_16Jan2012 = [
+  ('/DoubleElectron/Run2011A-16Jan2012-v1/AOD','MySCAnalysis/runData/jsonFiles/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON_v2.txt')
+  ,('/DoubleElectron/Run2011B-16Jan2012-v1/AOD','MySCAnalysis/runData/jsonFiles/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON_v2.txt')
+]
+
+dsEMu_better = [
+  ('/MuEG/Run2011A-03Oct2011-v1/AOD','MySCAnalysis/runData/jsonFiles/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt')
+  ,('/MuEG/Run2011B-PromptReco-v1/AOD','MySCAnalysis/runData/jsonFiles/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt')
+]
 #######################
 diMu =  datasetTools.createDatasampleFile(os.getenv('CMSSW_BASE')+'/MySCAnalysis/runData/diMuonData.py')
 diEl =  datasetTools.createDatasampleFile(os.getenv('CMSSW_BASE')+'/MySCAnalysis/runData/diElectronData.py')
 MuE =   datasetTools.createDatasampleFile(os.getenv('CMSSW_BASE')+'/MySCAnalysis/runData/MuonElectronData.py')
 diMu_16Jan2012 = datasetTools.createDatasampleFile(os.getenv('CMSSW_BASE')+'/MySCAnalysis/runData/diMuonData_16Jan2012.py')
+diEl_16Jan2012 = datasetTools.createDatasampleFile(os.getenv('CMSSW_BASE')+'/MySCAnalysis/runData/diElectronData_16Jan2012.py')
+MuE_better =   datasetTools.createDatasampleFile(os.getenv('CMSSW_BASE')+'/MySCAnalysis/runData/MuonElectronData_better.py')
 ##############
 for ds in dsDoubleMu:
  diMu.addDataset(ds)
@@ -49,8 +60,12 @@ for ds in dsDoubleE:
   diEl.addDataset(ds)
 for ds in dsEMu:
   MuE.addDataset(ds)
-for ds in dsDoubleMu_16Jan2012:
-  diMu_16Jan2012.addDataset(ds)
+for ds,j in dsDoubleMu_16Jan2012:
+  diMu_16Jan2012.addDataset(ds,goldenJson=j)
+for ds,j in dsDoubleE_16Jan2012:
+  diEl_16Jan2012.addDataset(ds,goldenJson=j)
+for ds,j in dsEMu_better:
+  MuE_better.addDataset(ds,goldenJson=j)
 #########################
 diLepton =  datasetTools.createDatasampleFile(os.getenv('CMSSW_BASE')+'/MySCAnalysis/runData/diLeptonData.py')
 diLepton.datasets =  diMu.datasets + diEl.datasets + MuE.datasets

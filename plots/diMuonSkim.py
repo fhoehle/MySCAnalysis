b9a2980da5c5798e75c15f30e4d6879c18a15ee8
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-08-10 12:11:02 +0200

    starting data mc comp

---
+++ plots/diMuonSkim.py
@@ -0,0 +1,9 @@
+dataJobs = "/disk1/hoehle/CMSSW_4_2_8_patch7/MySCAnalysis/runData/Data_DiMuonOnly_DoubleMuRunB_Skim_Grid__2014-08-03_19-05-48/crabJobResults.JSON"
+bkgJobs = "/disk1/hoehle/CMSSW_4_2_8_patch7/MySCAnalysis/testAnalysis/OtherBackgrounds_DiMuonOnly_Skim_Grid_2014-08-05_15-22-53/crabJobResults.JSON"
+
+ttbarBkgCorr = "/disk1/hoehle/CMSSW_4_2_8_patch7/MySCAnalysis/runSignal/TTBkg_DiMuonOnly_Skim_Grid_2014-08-03_18-55-45/crabJobResults.json"
+ttbarSigCorr = "/disk1/hoehle/CMSSW_4_2_8_patch7/MySCAnalysis/runSignal/TTSignal_DiMuonOnly_Skim_Grid__2014-08-03_17-06-49/crabJobResults.json"
+
+ttbarSigNoCorr = "/disk1/hoehle/CMSSW_4_2_8_patch7/MySCAnalysis/runSignal/TTnoCorrSigSamples_DiMuonOnly_Skim_Grid__2014-08-06_16-54-29/crabJobResults.json"
+ttbarBkgNoCorr = "/disk1/hoehle/CMSSW_4_2_8_patch7/MySCAnalysis/runSignal/TTnoCorrSamples_DiMuonOnly_Skim_Grid__2014-08-06_15-02-12/crabJobResults.json"
+
